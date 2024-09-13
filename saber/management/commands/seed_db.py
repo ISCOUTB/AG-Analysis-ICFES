6f6d291524af458cc1c59d2217ddf2c8c0b22f66
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2024-09-13 12:42:33 -0500

    Instead of using a separated file ... Now using a django command instead to handle the seed of the database also, there's a argparser, to customize the behavior of the script So far, it only gets the file, sets the num_threads and iterates over the .csv

--- saber/management/commands/seed_db.py
+++ saber/management/commands/seed_db.py
@@ -1,8 +1,45 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandParser
+from concurrent.futures import ThreadPoolExecutor
+import pandas as pd
+import os
+from django.conf import settings
 
 
 class Command(BaseCommand):
     help = 'Populate the database with the data'
 
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument('-Ft', '--file-type',
+                            choices=['Saber11', 'SaberPro'],
+                            nargs='?',
+                            default='Saber11',
+                            help='File to be procesed')
+
+        parser.add_argument('-C', '--chunksize', type=int, nargs='?',
+                            default=100000, help='Chunksize to be used')
+
     def handle(self, *args, **options):
-        self.stdout.write('Seed was called')
+        file_type: str = options['file_type']
+
+        if file_type == 'Saber11':
+            csv_file: str = os.path.join(
+                settings.BASE_DIR, "data", "Resultados11.csv")
+
+        if file_type == 'SaberPro':
+            csv_file: str = os.path.join(
+                settings.BASE_DIR, "data", "ResultadosPro.csv")
+
+        if not os.path.exists(csv_file):
+            self.stdout.write(self.style.ERROR('[ Error ] File not found'))
+
+        chunksize: int = options['chunksize']
+
+        num_threads: int | None = os.cpu_count()
+
+        data_iterator = pd.read_csv(csv_file, chunksize=chunksize)
+
+        self.stdout.write(self.style.HTTP_INFO(
+            f"[ Info ] Using ( Threads ) ( {num_threads} )"))
+
+        with ThreadPoolExecutor(max_workers=num_threads) as executor:
+            executor.map('', data_iterator)
