5e5f21ad350a59e4505a879918d8e48e9fa34189
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2024-09-14 19:48:12 -0500

    Cleaning the data First steps, the data is cleaned now (No null values) Which reduced the volume by a lot So ..., the next step is to parse it to the db schema

--- saber/management/commands/seed_db.py
+++ saber/management/commands/seed_db.py
@@ -1,8 +1,14 @@
+# ! Not using this
+
+
 from django.core.management.base import BaseCommand, CommandParser
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 import pandas as pd
 import os
 from django.conf import settings
+from pandas.io.parsers import TextFileReader
+import multiprocessing
+import numpy as np
 
 
 class Command(BaseCommand):
@@ -18,6 +24,23 @@ class Command(BaseCommand):
         parser.add_argument('-C', '--chunksize', type=int, nargs='?',
                             default=100000, help='Chunksize to be used')
 
+    def clean_chunk(self, df: pd.DataFrame) -> None:
+        df = df.dropna()
+        df = df.drop_duplicates()
+
+        return df
+
+    def process_chunk(self, chunk: pd.DataFrame) -> None:
+        with ThreadPoolExecutor() as executor:
+            sub_chunks = np.array_split(chunk, os.cpu_count())
+            cleaned_sub_chunks = list(
+                executor.map(self.clean_chunk, sub_chunks))
+
+        cleaned_chunk = pd.concat(cleaned_sub_chunks)
+
+        self.stdout.write(self.style.SUCCESS(
+            f'Processed chunk of size {len(cleaned_chunk)}'))
+
     def handle(self, *args, **options):
         file_type: str = options['file_type']
 
@@ -25,21 +48,29 @@ class Command(BaseCommand):
             csv_file: str = os.path.join(
                 settings.BASE_DIR, "data", "Resultados11.csv")
 
-        if file_type == 'SaberPro':
+        elif file_type == 'SaberPro':
             csv_file: str = os.path.join(
                 settings.BASE_DIR, "data", "ResultadosPro.csv")
 
+        else:
+            self.stdout.write(self.style.ERROR("[ Error ] Invalid file type"))
+            return
+
         if not os.path.exists(csv_file):
             self.stdout.write(self.style.ERROR('[ Error ] File not found'))
+            return
 
         chunksize: int = options['chunksize']
-
-        num_threads: int | None = os.cpu_count()
-
-        data_iterator = pd.read_csv(csv_file, chunksize=chunksize)
+        num_processes: int = multiprocessing.cpu_count()
 
         self.stdout.write(self.style.HTTP_INFO(
-            f"[ Info ] Using ( Threads ) ( {num_threads} )"))
+            f"[ Info ] Using {num_processes} processes"))
 
-        with ThreadPoolExecutor(max_workers=num_threads) as executor:
-            executor.map('', data_iterator)
+        data_iterator: TextFileReader = pd.read_csv(
+            csv_file, chunksize=chunksize)
+
+        with ProcessPoolExecutor(max_workers=num_processes) as executor:
+            executor.map(self.process_chunk, data_iterator)
+
+        self.stdout.write(self.style.SUCCESS(
+            '[ Success ] Processing completed'))    
