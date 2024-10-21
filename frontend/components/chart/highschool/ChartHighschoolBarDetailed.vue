e8109c97bbb7bfb7af23bcfe2fbb96c703d3e9cb
MauroGonzalez51 <mauroalonso.g.f2004@gmail.com>
2024-10-21 15:22:00 -0500

    Kinda designing the part to select by institution and period

--- frontend/components/chart/highschool/ChartHighschoolBarDetailed.vue
+++ frontend/components/chart/highschool/ChartHighschoolBarDetailed.vue
@@ -3,13 +3,16 @@
 </script>
 
 <template>
-    <template
-        v-for="highschoolId in Object.keys(highschoolGroupBy)"
-        :key="highschoolId"
-    >
-        <ChartHighschoolBarDetailedCollapsible
-            :highschool-id="highschoolId"
-            :items="highschoolGroupBy[highschoolId]"
-        />
-    </template>
+    <ScrollArea class="h-64 w-full rounded-md border-2 border-emerald-500 p-4">
+        <template
+            v-for="highschoolId in Object.keys(highschoolGroupBy)"
+            :key="highschoolId"
+        >
+            <ChartHighschoolBarDetailedCollapsible
+                :highschool-id="highschoolId"
+                :items="highschoolGroupBy[highschoolId]"
+                class="mb-4 last:mb-0"
+            />
+        </template>
+    </ScrollArea>
 </template>
